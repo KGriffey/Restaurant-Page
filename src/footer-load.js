a82372da6810f8bedac30c528b7e738ffa82379f
KGriffey <griffey.kyle@gmail.com>
2022-01-17 15:56:45 -0700

    Saving

--- src/footer-load.js
+++ src/footer-load.js
@@ -1,6 +1,19 @@
 function loadFooter() {
-    const footer = document.querySelector('footer');
-    footer.innerHTML = 'Copyright &copy 2022 Kyle Griffey';
+    // Grab the content container in the DOM
+    const content = document.querySelector('content');
+
+    // Create the footer
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    footer.setAttribute('id', 'footer');
+
+    const footerText = document.createElement('p');
+    footerText.classList.add('footer-text');
+    footerText.textContent = 'Copyright &copy 2022 Kyle Griffey';
+
+    // Append the footer to the DOM
+    footer.appendChild(footerText);
+    content.appendChild(footer);
 }
 
 export {loadFooter};
